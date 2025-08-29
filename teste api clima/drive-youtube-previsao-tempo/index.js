33d1a1e2320e3060ced10b1ccd3d79bdf7894be6
Eduarda de Barros Correia <dudabc162@gmail.com>
2025-08-28 22:14:29 -0300

    começando js

--- teste api clima/drive-youtube-previsao-tempo/index.js
+++ teste api clima/drive-youtube-previsao-tempo/index.js
@@ -1,6 +1,9 @@
-const API_KEY = ''
+const API_KEY = '10b8506bd000d5f23e2894b06b8e6ced'
 
 function getData(local) {
   const route = `https://api.openweathermap.org/data/2.5/weather?q=${local}&lang=pt_br&units=metric&appid=${API_KEY}`
   return fetch(route).then(response => response.json())
 }
+  document.querySelector('form').addEventListener('submit', function(){
+    const value = document.querySelector('[name="local"]').value
+  })
